b12be1cfe6b084bd40bc9e86b746c3ad664303fe
Meaghan Spedden <mspedden@FIL.ION.UCL.AC.UK>
2023-10-12 10:35:33 +0100

    new paths

--- source/getSpinalMetaData.m
+++ source/getSpinalMetaData.m
@@ -5,15 +5,15 @@ if strcmp(subID,'116')
     [mydir '\sub-OP00116\ses-001\meg\pplhandoe1000msfdfflo45hi5sub-OP00116_ses-001_task-staticleft_run-001_meg.mat']);
 
     metadat.posfile=[mydir '\sub-OP00116\ses-001\meg\sub-OP00116_ses-001_positions_new.tsv'];
-    metadat.backshape=['D:\OP00116_experiment\cast space\OP0016_seated.stl'];
-    metadat.cyl=['D:\OP00116_experiment\cast space\cylinder_good_space.stl'];
+    metadat.backshape=[mydir '\OP00116_experiment\cast space\OP0016_seated.stl'];
+    metadat.cyl=[mydir '\OP00116_experiment\cast space\cylinder_good_space.stl'];
     metadat.brainchan_labels={'19','1B', 'MW', 'MY','OK','MZ','35','DI','MU','DQ','OH','DG','A1'};
     metadat.badchans={};
     metadat.dpath=[mydir '\sub-OP00116\ses-001\meg'];
     metadat.savepath=[mydir '\Coh_results00116'];
-    metadat.layoutfile='D:\OP00116_experiment\lay_head_116';
-    metadat.sensorstl='D:\OP00116_experiment\cast space\Neckcoil_V3_Sensor_Placements.stl';
-
+    metadat.layoutfile=[mydir '\OP00116_experiment\lay_head_116'];
+    metadat.sensorstl=[mydir '\OP00116_experiment\cast space\sensorsOnly.stl'];
+    metadat.figsavedir=[mydir '\FiguresForPaper'];
 
 elseif strcmp(subID,'122')
     metadat.filenames=strvcat([mydir '\sub-OP00122\ses-001\meg\pprhandoe1000msfdfflo45hi5ds_sub-OP00122_ses-001_task-static_right_run-002_meg.mat'],...
@@ -26,7 +26,7 @@ elseif strcmp(subID,'122')
     metadat.dpath=[mydir '\sub-OP00122\ses-001\meg'];
     metadat.savepath=[mydir '\Coh_results00122'];
     metadat.layoutfile='D:\OP00122_experiment\lay_head_122';
-    metadat.sensorstl='D:\OP00122_experiment\cast space\Neckcoil_V3_Sensor_Placements.stl';
+    metadat.sensorstl='D:\OP00122_experiment\cast space\sensorsOnly.stl';
 
 
 
@@ -42,7 +42,7 @@ elseif strcmp(subID,'123')
     metadat.dpath=[mydir '\sub-OP00124\ses-001\meg'];
     metadat.savepath=[mydir '\Coh_results00123'];
     metadat.layoutfile='D:\OP00123_experiment\lay_head_123';
-    metadat.sensorstl='D:\OP00123_experiment\cast space\Neckcoil_V3_Sensor_Placements.stl';
+    metadat.sensorstl='D:\OP00123_experiment\cast space\sensorsOnly.stl';
 
 elseif strcmp(subID,'136')
 metadat.filenames=strvcat([mydir 'sub-OP00124\ses-001\meg\pprhandoe1000msfdfflo45hi5pds_sub-OP00136_ses-001_task-staticright_run-002_meg.mat'],...
